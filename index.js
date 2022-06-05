b25c991f6d1fd718a5111668b4311b0bc6120d67
LonieCre <rbatoulime@gmail.com>
2022-06-05 19:13:39 +0000

    add redux

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import "expo-dev-client";
 import { registerRootComponent } from "expo";
 
 import App from "./App";
+import AppWrapper from './Test';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
+// It also ensures that whether you load the app in the Expo client or in a native build
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(AppWrapper);
